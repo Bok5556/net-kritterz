b537394cdcb0589f032d8a6141d74ead747ba950
Bok5556 <kyleg888000@gmail.com>
2026-09-20 15:34:12 +0200

    changed devlog/changelog and updated license version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
- function sayOne()  {alert("All Developmental news & updates can be found here.--------------------------------------------------\nNET KRITTERZ DEV LOG\n--------------------------------------------------\n\nNOW BUILDING - v0.4 | Days 22-24\nTitle: Handling Multiple Moods\n\nI learned how Else If statements work.\n\nPets can now react to multiple conditions instead of only one, making moods feel smarter and more dynamic.\n\n--------------------------------------------------\nNext Up: v0.5 | Days 25-27 - Data Objects\n--------------------------------------------------\n");}
+ function sayOne()  {alert("All Developmental news & updates can be found here.--------------------------------------------------\nv0.5 | Days 25-27\nTitle: Grouping Pet Facts\n\nI learned how to use JavaScript objects.\n\nObjects let me keep pet data together in one profile instead of using separate variables.\n\n--------------------------------------------------\nNext Up: v0.6 | Days 28-30 - Building the Profile Structure\n--------------------------------------------------\n<br>\n<br>");}
                                   
      function sayTwo() {alert("Get ready to adopt, raise, and love your new digital petz! Here is a sneak peek at what you can do:\n•Play fun mini-games to earn Kritter-Kash🤑🤑\n•Dress them up in snazzy and fun outfits💲💲💲\n•Buy them yummy treats🍬🍭🍖🍗  And keep them HAPPY😊😊  Coming soon! "); }  
- function sayThree() {alert("the Current GAME VERSION: 0.0.3");}
+ function sayThree() {alert("the Current GAME VERSION: 0.0.4");}
 
 function showEULA() {
       if (!localStorage.getItem("eulaAccepted")) {
